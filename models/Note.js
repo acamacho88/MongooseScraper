efb6ecb78ebf8b784351ce75fd95fa4980d3282a
Alex Camacho <alex.c@WalkMe.local>
2018-10-06 13:31:10 -0700

    got scraper to work on bbc news

---
+++ models/Note.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema({
+    title: String,
+    text: String
+})
+
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
